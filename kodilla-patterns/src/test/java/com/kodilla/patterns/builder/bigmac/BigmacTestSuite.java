466d0a687339165ad8cce0ebe60afdd3f44a2956
Karol Androsz <karol@androsz.pl>
2021-11-23 03:00:36 +0100

    15.5 Bigmac

--- kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -27,21 +27,3 @@ public class BigmacTestSuite {
     }
 
 }
-
-
-//    @Test
-//    void testPizzaNew() {
-//        //Given
-//        Pizza pizza = new Pizza.PizzaBuilder()
-//                .ingredient("Onion")
-//                .bottom("Thin")
-//                .sauce("Spicy")
-//                .ingredient("Ham")
-//                .ingredient("Mushrooms")
-//                .build();
-//        System.out.println(pizza);
-//        //When
-//        int howManyIngredients = pizza.getIngredients().size();
-//        //Then
-//        assertEquals(3, howManyIngredients);
-//    }
