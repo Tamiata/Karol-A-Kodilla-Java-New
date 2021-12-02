af0519d8c07110a851d52624a77704479000058e
Karol Androsz <karol@androsz.pl>
2021-12-03 00:03:08 +0100

    Failed to Load ApplicationContext

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -12,6 +12,8 @@ public final class Task {
     private String description;
     private Date created;
     private int duration;
+    private TaskFinancialDetails taskFinancialDetails;
+    private TaskList taskList;
 
     public Task() {
     }
@@ -46,31 +48,138 @@ public final class Task {
         return duration;
     }
 
-    private void setId(int id) {
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name= "TASKS_FINANCIALS_ID")
+    public TaskFinancialDetails getTaskFinancialDetails() {
+        return taskFinancialDetails;
+    }
+
+    public void setTaskFinancialDetails(TaskFinancialDetails taskFinancialDetails) {
+        this.taskFinancialDetails = taskFinancialDetails;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "TASKLISTS_ID")
+    public TaskList getTaskList() {
+        return taskList;
+    }
+
+    public void setTaskList(TaskList taskList) {
+        this.taskList = taskList;
+    }
+
+    public void setId(int id) {
         this.id = id;
     }
 
-    private void setDescription(String description) {
+    public void setDescription(String description) {
         this.description = description;
     }
 
-    private void setCreated(Date created) {
+    public void setCreated(Date created) {
         this.created = created;
     }
 
-    private void setDuration(int duration) {
+    public void setDuration(int duration) {
         this.duration = duration;
     }
-
-    @Override
-    public String toString() {
-        return "Task{" +
-                "id=" + id +
-                ", description='" + description + '\'' +
-                ", created=" + created +
-                ", duration=" + duration +
-                '}';
-    }
-
-
 }
+
+//package com.kodilla.hibernate.task;
+//
+//import javax.persistence.*;
+//import javax.validation.constraints.NotNull;
+//import java.util.Date;
+//
+//@Entity
+//@Table(name = "TASKS")
+//public final class Task {
+//
+//    private int id;
+//    private String description;
+//    private Date created;
+//    private int duration;
+//    private TaskFinancialDetails taskFinancialDetails;
+//    private TaskList taskList;
+//
+//    public Task() {
+//    }
+//
+//    public Task(String description, int duration) {
+//        this.description = description;
+//        this.created = new Date();
+//        this.duration = duration;
+//    }
+//
+//    @Id
+//    @GeneratedValue
+//    @NotNull
+//    @Column(name = "ID", unique = true)
+//    public int getId() {
+//        return id;
+//    }
+//
+//    @Column(name = "DESCRIPTION")
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    @NotNull
+//    @Column(name="CREATED")
+//    public Date getCreated() {
+//        return created;
+//    }
+//
+//    @Column(name="DURATION")
+//    public int getDuration() {
+//        return duration;
+//    }
+//
+//    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+//    @JoinColumn(name = "TASKS_FINANCIALS_ID")
+//    public TaskFinancialDetails getTaskFinancialDetails() {
+//        return taskFinancialDetails;
+//    }
+//
+//    @ManyToOne
+//    @JoinColumn(name = "TASKLIST_ID")
+//    public TaskList getTaskList() {
+//        return taskList;
+//    }
+//
+//    private void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    private void setDescription(String description) {
+//        this.description = description;
+//    }
+//
+//    private void setCreated(Date created) {
+//        this.created = created;
+//    }
+//
+//    private void setDuration(int duration) {
+//        this.duration = duration;
+//    }
+//
+//    public void setTaskFinancialDetails(TaskFinancialDetails taskFinancialDetails) {
+//        this.taskFinancialDetails = taskFinancialDetails;
+//    }
+//
+//    public void setTaskList(TaskList taskList) {
+//        this.taskList = taskList;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Task{" +
+//                "id=" + id +
+//                ", description='" + description + '\'' +
+//                ", created=" + created +
+//                ", duration=" + duration +
+//                '}';
+//    }
+//
+//
+//}
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
@@ -1,14 +1,18 @@
 package com.kodilla.hibernate.task.dao;
 
 import com.kodilla.hibernate.task.Task;
+import com.kodilla.hibernate.task.TaskFinancialDetails;
+import com.kodilla.hibernate.task.TaskList;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -20,6 +24,8 @@ public class TaskDaoTestSuite {
     @Autowired
     private TaskDao taskDao;
 
+    @Autowired
+    private TaskListDao taskListDao;
 
     @Test
     void testTaskDaoSave() {
@@ -75,6 +81,49 @@ public class TaskDaoTestSuite {
         taskDao.deleteAll();
         System.out.println(taskDao.count());
 
+    }
+
+    @Test
+    void testTaskDaoSaveWithFinancialDetails() {
+        //Given
+        Task task = new Task(DESCRIPTION, 30);
+        task.setTaskFinancialDetails(new TaskFinancialDetails(new BigDecimal(120), false));
+
+        //When
+        taskDao.save(task);
+        int id = task.getId();
+
+        //Then
+        assertNotEquals(0, id);
+
+        //CleanUp
+        //taskDao.deleteById(id);
+    }
+
+    @Test
+    void testTaskListDaoSaveWithTasks() {
+        //Given
+        Task task = new Task("Test: Learn Hibernate", 14);
+        Task task2 = new Task("Test: Write some entities", 3);
+        TaskFinancialDetails tfd = new TaskFinancialDetails(new BigDecimal(20), false);
+        TaskFinancialDetails tfd2 = new TaskFinancialDetails(new BigDecimal(10), false);
+        task.setTaskFinancialDetails(tfd);
+        task2.setTaskFinancialDetails(tfd2);
+        TaskList taskList = new TaskList("LISTNAME", "ToDo tasks");
+        taskList.getTasks().add(task);
+        taskList.getTasks().add(task2);
+        task.setTaskList(taskList);
+        task2.setTaskList(taskList);
+
+        //When
+        taskListDao.save(taskList);
+        int id = taskList.getId();
+
+        //Then
+        assertNotEquals(0, id);
+
+        //CleanUp
+        //taskListDao.deleteById(id);
     }
 
 }
